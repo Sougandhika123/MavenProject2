42212f17a3b0fef21d046f75696825b5727533af
Sougandhika T N <SNMT720@MGTECHSOFT.IN>
2023-02-21 11:55:39 +0530

    add App.Java

---
+++ src/main/java/com/MavenProject3/app/App/App.java
@@ -0,0 +1,13 @@
+package com.MavenProject3.app.App;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
